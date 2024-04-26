466b3bfa33e5c629d26a8f1ff83b753a42e7b832
mustafa sen <mustafasen@tuta.io>
2024-04-27 00:37:09 +0300

    added leaflet

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import "./style/index.css"
+import L from "leaflet";
+
+const map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
